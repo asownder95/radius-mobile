bb10103833c62bb01b2b69a17a00a300a89feb71
Anoop Sownderraj <asownder95@gmail.com>
2018-11-16 17:36:09 -0800

    Implemented core application navigation

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { createStackNavigator } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import ListScreen from './client/screens/ListScreen';
+import Header from './client/components/Header';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+
+  }
   render() {
+    const props = {
+      events: this.state.events,
+    };
+    const tabNavigation = createMaterialBottomTabNavigator({
+      List: {
+        screen: (props) => <ListScreen {...props} />,
+        navigationOptions: {
+          tabBarIcon: ({ focused, tintColor }) => (<Ionicons name="ios-list" size={34} style={{color: 'white'}} />),
+          tabBarColor: '#592CEA'
+        }
+      },
+    });
+    
+    const RootStack = createStackNavigator({
+      Tabs: {
+        screen: tabNavigation,
+        navigationOptions: {
+          headerTitle: <Header />,
+          headerStyle: {
+            backgroundColor: '#592CEA',
+          },
+        },
+      },
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <RootStack screenProps={props} />
     );
   }
 }
